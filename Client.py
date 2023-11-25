2c223cb6f7e94ab571fba2ffc2dae2adb63b9ccc
yshic <tuanl799@gmail.com>
2023-11-25 16:42:23 +0700

    fix command line interpreter

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ import threading
 import os
 
 class Client:
-    def __init__(self, host = '127.0.0.1', port = 12345):
+    def __init__(self, host = socket.gethostname(), port = 22236):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
         self.files = {}
@@ -18,28 +18,53 @@ class Client:
                 if message.startswith('send'):
                     _, lname, addr = message.split()
                     # fetch file from addr
-                    pass
+                    self.fetch_file(lname, addr)
+                elif message.startswith('request'):
+                    _, lname = message.split()
+                    self.send_file(lname)
             except:
                 print("An error occured!")
                 self.client.close()
                 break
 
+    def fetch_file(self, lname, addr):
+        # create a new socket connection to the client with the file
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(addr)
+            s.send(f'request {lname}'.encode('ascii'))
+            data = s.recv(1024)
+            # write data to a file
+            with open(lname, 'wb') as f:
+                f.write(data)
+
+    def send_file(self, lname):
+        if lname in self.files:
+            with open(self.files[lname], 'rb') as f:
+                data = f.read()
+                self.client.send(data)
+
     def command_shell(self):
         while True:
             cmd = input('Enter command: ')
             if cmd.startswith('publish'):
-                _, lname, fname = cmd.split()
-                if os.path.isfile(lname):
-                    self.files[fname] = lname
-                    self.send(cmd)
-                else:
-                    print(f'File {lname} does not exist.')
+                try:
+                    _, lname, fname = cmd.split()
+                    if os.path.isfile(lname):
+                        self.files[fname] = lname
+                        self.send(cmd)
+                    else:
+                        print(f'File {lname} does not exist.')
+                except ValueError:
+                    print('Invalid command. The correct format is: publish [localname] [filename]')
             elif cmd.startswith('fetch'):
-                _, fname = cmd.split()
-                if fname in self.files:
-                    print(f'File {fname} is already in the local repository.')
-                else:
-                    self.send(cmd)
+                try:
+                    _, fname = cmd.split()
+                    if fname in self.files:
+                        print(f'File {fname} is already in the local repository.')
+                    else:
+                        self.send(cmd)
+                except ValueError:
+                    print('Invalid command. The correct format is: fetch [filename]')
             else:
                 print('Invalid command.')
 
@@ -50,3 +75,4 @@ if __name__ == "__main__":
 
     command_shell_thread = threading.Thread(target=client.command_shell)
     command_shell_thread.start()
+
--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 class Server:
-    def __init__(self, host = '127.0.0.1', port = 12345):
+    def __init__(self, host = socket.gethostname(), port = 22236):
         self.host = host
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,13 +23,38 @@ class Server:
                     lname, owner = self.files[fname]
                     owner.send(f'send {lname} to {client.getpeername()}'.encode('ascii'))
 
+    def command_shell(self):
+        while True:
+            cmd = input('Enter command: ')
+            if cmd.startswith('discover'):
+                _, hostname = cmd.split()
+                if hostname in self.clients:
+                    client = self.clients[hostname]
+                    client.send('list'.encode('ascii'))
+                else:
+                    print(f'Host {hostname} not found.')
+            elif cmd.startswith('ping'):
+                _, hostname = cmd.split()
+                if hostname in self.clients:
+                    client = self.clients[hostname]
+                    client.send('ping'.encode('ascii'))
+                else:
+                    print(f'Host {hostname} not found.')
+            else:
+                print('Invalid command.')
+
     def run(self):
         while True:
             client, addr = self.server.accept()
+            print(f'New connection from {addr}')
             self.clients[addr] = client
             thread = threading.Thread(target=self.handle, args=(client,))
             thread.start()
 
+            command_shell_thread = threading.Thread(target=self.command_shell)
+            command_shell_thread.start()
+   
 if __name__ == "__main__":
     server = Server()
     server.run()
+    
